3a1c2a76947617190311e41f847692921294e767
KienHaVan <hakien351998@gmail.com>
2023-09-09 15:06:19 +0700

    update

--- server/server.js
+++ server/server.js
@@ -27,6 +27,7 @@ app.use('/', require('./routes/root'));
 app.use('/auth', require('./routes/authRoutes'));
 app.use('/users', require('./routes/userRoutes'));
 app.use('/notes', require('./routes/noteRoutes'));
+console.log('first');
 
 app.all('*', (req, res) => {
   res.status(404);
